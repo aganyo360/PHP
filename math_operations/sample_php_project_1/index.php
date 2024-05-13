242a79157e6a952851645f5b0ac2d113b0d8617a
aganyo360 <wilsonaganyo@gmail.com>
2024-05-14 00:07:38 +0300

    added math methods

--- math_operations/sample_php_project_1/index.php
+++ math_operations/sample_php_project_1/index.php
@@ -1,72 +1,3 @@
-<!-- <?php
-// $name ='Brice wilson';
-// $email = 'wilsonaganyo@gmail.com';
-// $message = 'how are you today';
-
-
-// $age =22;
-// $users =2;
-
-// // float datatypes
-// $gpa =2.8;
-// $price =5.44;
-// $quantity = 8;
-// $rate = 4;
-
-
-// // boolean datatypes
-// $employed = true;
-// $total = $quantity * $price;
-// // echo "Hello {$name}<br> {$message} <br>{$email} <br> You are {$age} yeers old<br>";
-// // echo "there are {$users} users online<br>";
-// // echo "Your gpa is {$gpa}<br>";
-// // echo "Your pizza is \${$price} <br>";
-// // echo "the sale tax rate is {$rate}% <br>";
-// // echo "your emploment status is {$employed}"
-// echo " You ordered {$quantity} x Pizzas <br>";
-// echo " Your total bill is  \${$total} ";
-
-
-
-// ?> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- /Arithmentic operation -->
-<!-- 
-<?php
-
-// $x =12;
-// $y =10;
-// $z =null;
-
-//  echo $x % $y;
-
-?> -->
-
-
-
-<!-- //Increment operators -->
-<!-- <?php
-// $counter =4;
-// $counter++;
-// echo $counter
-?> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -75,11 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-<form action="total.php" method="post">
-    <label for="">Enter the amount of quantity you need</label><br>
-
-    <input type="text" name="quantity"> <br>
-    <input type="submit" value="Total">
-</form>
+    <form action="index.php" method="post">
+        <label for="">Enter the radius of the circle</label> <br>
+        <input type="text" name="r"><br>
+        <br>
+        <input type="submit" value="Calculate">
+       
+    </form>
+    
 </body>
 </html>
+<?php
+$r = $_POST["r"];
+
+$Circumference =null;
+$Area =null;
+$Circumference = pi()*2*$r;
+$Circumference = round($Circumference, 2);
+;$Area = pi()*($r**2);
+echo $Circumference;
+echo " <br> Area= {$Area}"
+?>
