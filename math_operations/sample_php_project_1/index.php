0702a14b781daa4cdf318126d5a1ed0493bfabe7
aganyo360 <wilsonaganyo@gmail.com>
2024-05-13 19:23:37 +0300

    added math and special var projects

---
+++ math_operations/sample_php_project_1/index.php
@@ -0,0 +1,85 @@
+<!-- <?php
+// $name ='Brice wilson';
+// $email = 'wilsonaganyo@gmail.com';
+// $message = 'how are you today';
+
+
+// $age =22;
+// $users =2;
+
+// // float datatypes
+// $gpa =2.8;
+// $price =5.44;
+// $quantity = 8;
+// $rate = 4;
+
+
+// // boolean datatypes
+// $employed = true;
+// $total = $quantity * $price;
+// // echo "Hello {$name}<br> {$message} <br>{$email} <br> You are {$age} yeers old<br>";
+// // echo "there are {$users} users online<br>";
+// // echo "Your gpa is {$gpa}<br>";
+// // echo "Your pizza is \${$price} <br>";
+// // echo "the sale tax rate is {$rate}% <br>";
+// // echo "your emploment status is {$employed}"
+// echo " You ordered {$quantity} x Pizzas <br>";
+// echo " Your total bill is  \${$total} ";
+
+
+
+// ?> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- /Arithmentic operation -->
+<!-- 
+<?php
+
+// $x =12;
+// $y =10;
+// $z =null;
+
+//  echo $x % $y;
+
+?> -->
+
+
+
+<!-- //Increment operators -->
+<!-- <?php
+// $counter =4;
+// $counter++;
+// echo $counter
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="total.php" method="post">
+    <label for="">Enter the amount of quantity you need</label><br>
+
+    <input type="text" name="quantity"> <br>
+    <input type="submit" value="Total">
+</form>
+</body>
+</html>
---
+++ math_operations/sample_php_project_1/total.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$quantity = $_POST["quantity"] *20;
+echo " the total amount is\${$quantity}";
+
+?>
+</body>
+</html>
